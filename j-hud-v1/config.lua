b35741551a4e228471fc8a1ecd32602bb2170d32
thewolf1851 <103337228+thewolf1851@users.noreply.github.com>
2022-11-25 23:43:28 +0100

    Add files via upload

---
+++ j-hud-v1/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.WhenUse = true
+
+Config.WhenKey = true
+
+Config.UseLogo  = true
+
+Config.LogoURL = ("https://media.discordapp.net/attachments/1018534201550700665/1043514794096996422/png1.png?width=676&height=676")
